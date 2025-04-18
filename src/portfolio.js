60dd13f4f390b4194fda06ae442ec17a89a00ebe
Masrafi <53406617+masrafi115@users.noreply.github.com>
2025-04-18 15:05:49 +0600

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -381,7 +381,7 @@ const contactInfo = {
   title: emoji("Contact Me ☎️"),
   subtitle:
     "Discuss a project or just want to say hi? My Inbox is open for all.",
-  number: "+92-0000000000",
+  number: "+880-0000000000",
   email_address: "abdurrahmanrafi57@gmail.com"
 };
 
